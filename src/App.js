670b38fba8327f69b0317de909554c0d59cbfcdf
Brendan Frost <brendangfrost@gmail.com>
2023-12-19 13:51:57 -0600

    build fixes

--- src/App.js
+++ src/App.js
@@ -11,42 +11,51 @@ function App() {
             <div className="row">
                 <div className="col-md-2 col-sm-12 util-col">
                     <img id="logo" src="/img/logo-circle-only-transparent.png"></img>
-                    <br />
-                    <br />
-                    <span className="pre-title">&#123;Brendan&#125;</span>
+                    {/* <br />
+                    <br /> */}
+                </div>
+                <div className="col-md-2 title-col">
+                    <span className="pre-title">
+                        <span className="open-curly">&#123;</span>
+                        Brendan
+                        <span className="closed-curly">&#125;</span>
+                    </span>
                     <h1 onClick={() => setView('about')} className="main-title hover-text">Frost<span>ware</span></h1>
                 </div>
-                <div className="col-md-7 toolbar">
-                    {/* <span className="pointer-animation">
-                        <img className="pointer" src="./img/pointer-real.png"></img>
-                    </span> */}
+                {/* <div className="col-md-5 toolbar">
+                    
+                </div> */}
+                <div className="col-md-3 offset-md-5 cat-col">
+                    <img id="fun-img" src="/img/laptop-cat-1.png"></img>
+                </div>
+            </div>
+            <div className="row">
+                <div className="cold-md-3 menu">
                     <div class="centralizer">
-                        <span onClick={() => setView('engineer')} className="toolbar-item-parent">
+                        {/* <span className="pointer-animation">
+                        <img className="pointer" src="./img/pointer-real.png"></img>
+                        </span> */}
+                        <div onClick={() => setView('engineer')} className="toolbar-item-parent">
                             <span className="open-curly">&#123;</span>
                             Software Portfolio
                             <span className="closed-curly">&#125;</span>
-                        </span>
-                        <span onClick={() => setView('webdev')} className="toolbar-item-parent">
+                        </div>
+                        <div onClick={() => setView('webdev')} className="toolbar-item-parent">
                             <span className="open-curly">&#123;</span>
                             Website Portfolio
                             <span className="closed-curly">&#125;</span>
-                        </span>
-                        <span onClick={() => setView('fiction')} className="toolbar-item-parent">
+                        </div>
+                        <div onClick={() => setView('fiction')} className="toolbar-item-parent">
                             <span className="open-curly">&#123;</span>
                             Who I Am
                             <span className="closed-curly">&#125;</span>
-                        </span>
+                        </div>
                         {/* <span onClick={() => setView('engineer')} className="toolbar-item-parent">Senior Software Engineer</span>
                         <span onClick={() => setView('webdev')} className="toolbar-item-parent">Website & App Creator</span>
                         <span onClick={() => setView('fiction')} className="toolbar-item-parent">Fiction & Article Author</span> */}
                     </div>
                 </div>
-                <div className="col-md-3 cat-col">
-                    <img id="fun-img" src="/img/laptop-cat-1.png"></img>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-10 col-sm-12">
+                <div className="col-md-9 col-sm-12">
                     {/* <DynamicContent view={view} /> */}
                 </div>
             </div>
