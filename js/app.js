742d3a983408da17ce6b801cd7b52ba928b321fc
Brendan Frost <brendangfrost@gmail.com>
2023-12-20 14:14:19 -0600

    basics of raw OOP + nilla JS + libraries build

--- js/app.js
+++ js/app.js
@@ -1,3 +1,70 @@
-function setView() {
+$(document).ready(function() {
     
-}
+    // console.log('inside doc ready...', Object.keys(_));
+
+    class App {
+
+        constructor() {
+            this.config = {
+                setView: 'engineer'
+            }
+        }
+        
+        setView(view, target) {
+            console.log('setView(): ', view);
+            this.config.setView = view;
+            console.log('setView() target: ', target);
+            $('.subsection')
+                .removeClass('perma')
+                .hide();
+            const tarjay = $('.' + view)
+            tarjay.show()
+            setTimeout(() => {
+                tarjay.addClass('perma');
+            }, 50)
+                
+                // .addClass('perma');
+            // $(target).addClass('perma');
+        }
+    
+    }
+
+    const app = new App();
+
+    $('.subsection.engineer')
+        .show()
+        .addClass('perma');
+
+    $('.toolbar-item-parent').click(e => {
+        const classList = e?.target?.classList;
+        const snatchedClass = classList[1];
+        app.setView(snatchedClass, e?.target);
+    });
+
+    $('')
+});
+
+// class App {
+
+//     constructor() {
+//         this.config = {
+//             setView: 'engineer'
+//         }
+//     }
+    
+//     setView(view) {
+//         console.log('aspiajfkl');
+//         this.config.setView = view;
+//         console.log('view = ', this.config.setView);
+        
+//     }
+
+// }
+
+// function setView(view) {
+//     app.setView(view);
+// }
+
+// (function createApp() {
+//     app = new App();
+// })()
